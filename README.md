3166c87d2f58433cdff9cd7c224ae63608a34bb8
AJ-SM <anujsidam1108@gmail.com>
2025-08-10 13:55:54 +0530

    added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Chip8-Emulation
 Trying To make Chip8
+
+Reference : [text](https://multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter/)
--- modules/def_memory.py
+++ modules/def_memory.py
@@ -4,18 +4,25 @@ from typing import * # Remove this to specifinces Before Pushing anywhere
 # Some CPU sepcific initial data
 max_memo:int = 4
 no_regs:int = 16
+stackLength:int = 16
+no_keys:int = 16
 
 ''' Why uint8 ?
     Since our Each Instruction is of 1 byte (8bits) so we need some data type to store 2^8 (256) 
     instruction and uint8 = UnsignedIntegerOf8bits (0~255)- (00~FF) seems to be the great choice  '''
 
 memory = np.array(max_memo*1024).astype(np.uint8)
+stack = np.array(stackLength).astype(np.uint8)
+hex_keypad = np.array(16)
 
 ''' since we only need to point form 0 ~ 4095 *(000~FFF) using 16bits is bit waste of memory
     because 12bits which we need not there in nmpy..'''
 
 reg_index= np.uint16(0)
 reg_pc= np.uint16(0) 
+reg_delayTimer = np.uint16(0)
+reg_soundTimer = np.uint16(0) # Won't Be using it probably
+reg_stackPointer = np.uint(0) # just indexs of the stack 
 
 
 '''
@@ -27,6 +34,6 @@ reg_pc= np.uint16(0)
 
 
 
-print(type(reg_index))
+
 
 # Quick Note : Everything over here is zero Indexed
